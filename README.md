10a1d7e53d324a4800bc8de98a64deb6ed466b25
Must <nm.ekinci@gmail.com>
2023-11-21 20:26:15 +0100

    gitignore npm init -y readme dbconnecton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # blogapp-api
+
+npm init -y
+npm i express-async-errors
+created a .gitignore file and filled 
---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict"
+// Node.js - Express.js Blog-API 
+
+const express = require('express')
+const app = express()
+
+require('dotenv').config() // for using .env variables
+const PORT = process.env?.PORT || 8000
+
+require('express-async-errors') // for async errors to errorHandler
+
+app.use(express.json()) //for ability to get json
+
+
+//Home Path for API service
+app.all('/api', (req,res) => {
+    res.send({
+        error: false,
+        message: "It's Blog API service, Wellcome..",
+        isLogin: req.isLogin,
+        user: req.user
+    })
+})
+
+//for running server
+app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
