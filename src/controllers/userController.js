1c9326f640ba8bd37d7fd5e1f81048aa7786a9eb
Must <nm.ekinci@gmail.com>
2023-11-22 14:50:32 +0100

    userControl & findsearchsortpage

---
+++ src/controllers/userController.js
@@ -0,0 +1,112 @@
+"use strict";
+
+const User = require("../models/userModel");
+const {
+  getModelList,
+  getModelListDetails,
+} = require("../middlewares/findSearchSortPage");
+
+module.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+    const data = await getModelList(User);
+
+    res.status(200).send({
+      error: false,
+      count: data.length,
+      result: data,
+      details: await getModelListDetails(User),
+    });
+  },
+  read: async (req, res) => {
+    /*
+        #swagger.tags = ["Users"]
+        #swagger.summary = "Get Single User"
+    */
+    const data = await User.findOne({ _id: req.params.id });
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+  create: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "name": "test",
+                    "lastName": "test",
+                    "isAdmin": false,
+                    "isActive": true,
+                    "isGueast":false,
+                },
+            }
+    */
+    const data = await User.create(req.body);
+
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      result: data,
+    });
+  },
+  update: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "name": "test",
+                    "lastName": "test",
+                    "isAdmin": false,
+                    "isActive": true,
+                    "isGueast":false,
+                },
+            }
+    */
+    const data = await User.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
+
+    res.status(200).send({
+      error: false,
+      data,
+      new: await User.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+    */
+    const data = await User.deleteOne({ _id: req.params.id });
+
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
